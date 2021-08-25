330cee75eec913672e1cc36ad8983459d086700b
Robin Bette <robinbette@hotmail.com>
2021-08-25 14:19:08 +0200

    unsubscribe

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { ProductService } from "../product.service";
 
 @Component({
   templateUrl: "./product-shell.component.html",
 })
-export class ProductShellComponent implements OnInit {
+export class ProductShellComponent implements OnInit, OnDestroy {
   pageTitle: string = "Products";
   monthCount: number;
+
+  sub: Subscription;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.selectedProductChanges$.subscribe((selectedProduct) => {
-      if (selectedProduct) {
-        const start = new Date(selectedProduct.releaseDate);
-        const now = new Date();
+    this.sub = this.productService.selectedProductChanges$.subscribe(
+      (selectedProduct) => {
+        if (selectedProduct) {
+          const start = new Date(selectedProduct.releaseDate);
+          const now = new Date();
 
-        this.monthCount =
-          now.getMonth() -
-          start.getMonth() +
-          12 * (now.getFullYear() - start.getFullYear());
-      } else {
-        this.monthCount = 0;
+          this.monthCount =
+            now.getMonth() -
+            start.getMonth() +
+            12 * (now.getFullYear() - start.getFullYear());
+        } else {
+          this.monthCount = 0;
+        }
       }
-    });
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
