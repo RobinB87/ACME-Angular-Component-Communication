08ef89a3942ff58876273a208bbbd712ff0477bd
Robin Bette <robinbette@hotmail.com>
2021-08-25 11:53:03 +0200

    Add simple 'property bag service' for when no ngrx is used

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,36 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from "../shared/shared.module";
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './edit/product-edit.component';
+import { ProductListComponent } from "./product-list.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { ProductEditComponent } from "./edit/product-edit.component";
 
-import { ProductService } from './product.service';
-import { ProductEditGuard } from './edit/product-edit-guard.service';
+import { ProductService } from "./product.service";
+import { ProductEditGuard } from "./edit/product-edit-guard.service";
+import { ProductParameterService } from "./product-parameter.service";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: ProductListComponent },
-      { path: ':id', component: ProductDetailComponent },
+      { path: "", component: ProductListComponent },
+      { path: ":id", component: ProductDetailComponent },
       {
-        path: ':id/edit',
-        canDeactivate: [ ProductEditGuard ],
-        component: ProductEditComponent
-      }
-    ])
+        path: ":id/edit",
+        canDeactivate: [ProductEditGuard],
+        component: ProductEditComponent,
+      },
+    ]),
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
   ],
-  providers: [
-    ProductService,
-    ProductEditGuard
-  ]
+  providers: [ProductService, ProductEditGuard, ProductParameterService],
 })
-export class ProductModule { }
+export class ProductModule {}
