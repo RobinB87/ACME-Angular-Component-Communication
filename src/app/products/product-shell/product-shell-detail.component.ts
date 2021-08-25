fd00215fa2db89db545a587392b915fb5cc585a1
Robin Bette <robinbette@hotmail.com>
2021-08-25 13:35:46 +0200

    Use different layout to display concurrent components and keep state in sync

--- src/app/products/product-shell/product-shell-detail.component.ts
+++ src/app/products/product-shell/product-shell-detail.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { IProduct } from '../product';
+import { Component } from "@angular/core";
+import { ProductService } from "../product.service";
+import { IProduct } from "../product";
 
 @Component({
-    selector: 'pm-product-shell-detail',
-    templateUrl: './product-shell-detail.component.html'
+  selector: "pm-product-shell-detail",
+  templateUrl: "./product-shell-detail.component.html",
 })
-export class ProductShellDetailComponent implements OnInit {
-    pageTitle: string = 'Product Detail';
+export class ProductShellDetailComponent {
+  pageTitle: string = "Product Detail";
 
-    constructor(private productService: ProductService) { }
+  get product(): IProduct | null {
+    return this.productService.currentProduct;
+  }
 
-    ngOnInit() {
-    }
-
+  constructor(private productService: ProductService) {}
 }
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -10,12 +10,15 @@ import { ProductEditComponent } from "./edit/product-edit.component";
 import { ProductService } from "./product.service";
 import { ProductEditGuard } from "./edit/product-edit-guard.service";
 import { ProductParameterService } from "./product-parameter.service";
+import { ProductShellComponent } from "./product-shell/product-shell.component";
+import { ProductShellListComponent } from "./product-shell/product-shell-list.component";
+import { ProductShellDetailComponent } from "./product-shell/product-shell-detail.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: "", component: ProductListComponent },
+      { path: "", component: ProductShellComponent },
       { path: ":id", component: ProductDetailComponent },
       {
         path: ":id/edit",
@@ -28,6 +31,9 @@ import { ProductParameterService } from "./product-parameter.service";
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
+    ProductShellComponent,
+    ProductShellListComponent,
+    ProductShellDetailComponent,
   ],
   providers: [ProductService, ProductEditGuard, ProductParameterService],
 })
