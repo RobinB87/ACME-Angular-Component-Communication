9a24c3c469d921a3eaba1d89b87f411d8bfbc011
Robin Bette <robinbette@hotmail.com>
2021-08-25 12:05:04 +0200

    Add comments

--- src/app/products/product-parameter.service.ts
+++ src/app/products/product-parameter.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ProductParameterService {
+  // This service is only retained as long as it's component is loaded.
+  // When you register a service in for example the product list component,
+  // it is destroyed when for example opening the details component
+
+  // In this case it is registered the product module and its component's
   showImage: boolean;
   filterBy: string;
 
