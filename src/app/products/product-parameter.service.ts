c5aed42b2ba7544121ed12ddc71e7007335ecc92
Robin Bette <robinbette@hotmail.com>
2021-08-25 12:08:04 +0200

    Comments

--- src/app/products/product-parameter.service.ts
+++ src/app/products/product-parameter.service.ts
@@ -6,7 +6,13 @@ export class ProductParameterService {
   // When you register a service in for example the product list component,
   // it is destroyed when for example opening the details component
 
-  // In this case it is registered the product module and its component's
+  // In this case it is registered the product module and thus registered in the application's root
+  // Now it is available to ALL components, for the lifetime of the application
+
+  // When module is lazy loaded, this is different
+  // Services registered in a lazy loaded module are only available to
+  // components declared in that module
+  // When the module is loaded, the service is available for the lifetime of the application
   showImage: boolean;
   filterBy: string;
 
